2d51f7f5d298851c2188b37086bdb4c52cb782e7
OluMel2 <129649026+OluMel2@users.noreply.github.com>
2023-09-14 14:45:55 +0000

    [Add]RSpec搭載のためGemfile追記ファイルとフォルダ作成

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,15 +5,13 @@ class ListsController < ApplicationController
   end
 
   def create
-    # １.&2. データを受け取り新規登録するためのインスタンス作成
     @list = List.new(list_params)
-    # 3. データをデータベースに保存するためのsaveメソッド実行
-   if @list.save
-    # 4. トップ画面へリダイレクト
-    redirect_to list_path(@list.id)
-   else
-    render :new
-   end
+    if @list.save
+      flash[:notice] = "投稿が成功しました"
+      redirect_to list_path(@list.id)
+    else
+     render :new
+    end
   end
 
   def index
